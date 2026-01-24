c9c4f3b5323ec236c159ee5d4fbe2a003185d12e
kranti-napa <72197389+kranti-napa@users.noreply.github.com>
2026-01-25 04:15:36 +0530

    Update StoreResourceTest.java

--- src/test/java/com/fulfilment/application/monolith/stores/StoreResourceTest.java
+++ src/test/java/com/fulfilment/application/monolith/stores/StoreResourceTest.java
@@ -6,19 +6,34 @@ import static org.hamcrest.Matchers.*;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.http.ContentType;
-import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 class StoreResourceTest {
 
-    @Inject
-    EntityManager entityManager;
-
     @InjectMock
     LegacyStoreManagerGateway legacyStoreManagerGateway;
+
+    // ---------- helpers ----------
+
+    private Long createStore(String name, int qty) {
+        Store store = new Store();
+        store.name = name;
+        store.quantityProductsInStock = qty;
+
+        Response response =
+            given()
+                .contentType(ContentType.JSON)
+                .body(store)
+            .when()
+                .post("/store")
+            .then()
+                .statusCode(201)
+                .extract().response();
+
+        return response.jsonPath().getLong("id");
+    }
 
     // ---------- GET ----------
 
@@ -43,7 +58,6 @@ class StoreResourceTest {
     // ---------- POST ----------
 
     @Test
-    @Transactional
     void shouldCreateStoreSuccessfully() {
         Store store = new Store();
         store.name = "Created Store";
@@ -79,9 +93,8 @@ class StoreResourceTest {
     // ---------- PUT ----------
 
     @Test
-    @Transactional
     void shouldUpdateStoreSuccessfully() {
-        Store store = persistStore("Old", 5);
+        Long id = createStore("Old", 5);
 
         Store updated = new Store();
         updated.name = "Updated";
@@ -91,94 +104,8 @@ class StoreResourceTest {
             .contentType(ContentType.JSON)
             .body(updated)
         .when()
-            .put("/store/" + store.id)
+            .put("/store/" + id)
         .then()
             .statusCode(200)
             .body("name", equalTo("Updated"))
-            .body("quantityProductsInStock", equalTo(20));
-    }
-
-    @Test
-    void shouldReturnBadRequestWhenUpdatingWithNullBody() {
-        given()
-            .contentType(ContentType.JSON)
-        .when()
-            .put("/store/1")
-        .then()
-            .statusCode(400)
-            .body(containsString("Store Name was not set"));
-    }
-
-    // ---------- PATCH ----------
-
-    @Test
-    @Transactional
-    void shouldPatchOnlyName() {
-        Store store = persistStore("Initial", 50);
-
-        Store patch = new Store();
-        patch.name = "Patched";
-
-        given()
-            .contentType(ContentType.JSON)
-            .body(patch)
-        .when()
-            .patch("/store/" + store.id)
-        .then()
-            .statusCode(200)
-            .body("name", equalTo("Patched"))
-            .body("quantityProductsInStock", equalTo(50));
-    }
-
-    @Test
-    void shouldReturnBadRequestWhenPatchBodyIsNull() {
-        given()
-            .contentType(ContentType.JSON)
-        .when()
-            .patch("/store/1")
-        .then()
-            .statusCode(400)
-            .body(containsString("Request body was empty"));
-    }
-
-    // ---------- DELETE ----------
-
-    @Test
-    @Transactional
-    void shouldDeleteStoreSuccessfully() {
-        Store store = persistStore("Delete Me", 1);
-
-        given()
-            .when()
-            .delete("/store/" + store.id)
-        .then()
-            .statusCode(204);
-
-        given()
-            .when()
-            .get("/store/" + store.id)
-        .then()
-            .statusCode(404);
-    }
-
-    @Test
-    void shouldReturn404WhenDeletingNonExistingStore() {
-        given()
-            .when()
-            .delete("/store/999999")
-        .then()
-            .statusCode(404);
-    }
-
-    // ---------- helper ----------
-
-    private Store persistStore(String name, int qty) {
-        Store store = new Store();
-        store.name = name;
-        store.quantityProductsInStock = qty;
-        entityManager.persist(store);
-        entityManager.flush();
-        entityManager.clear();
-        return store;
-    }
-}
+            .body("quantityProductsInStock", equalTo(20)
